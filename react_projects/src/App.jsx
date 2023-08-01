df2e76c836f081114fcb21e891befb438073f7f7
cloudgustPro <cloudgust.pro@gmail.com>
2023-08-01 15:18:23 +0300

    NavBar

--- react_projects/src/App.jsx
+++ react_projects/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import NavBar from './components/NavBar';
 
 const App = ()=>{
-  return <div class='text-lg'>
-    test
-  </div>
+  return <div>
+            <NavBar/>
+         </div>
 }
 
 export default App;
---
+++ react_projects/src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const NavBar = ()=>{
+    return (
+            <div className=''> 
+                <ul className='flex justify-end m-4'>
+                    <li className='pr-2'><a href="http://#"></a>Home</li>
+                    <li className='pr-2'><a href="http://#"></a>Home</li>
+                    <li className='pr-2'><a href="http://#"></a>Home</li>
+                    <li className='pr-2'><a href="http://#"></a>Home</li>
+                </ul>
+            </div>
+           )
+}
+export default NavBar;
